48c5dec21fa3a2cda5eeea28e901eca6ad7264a5
Daisy Wu <daisy43097@gmail.com>
2024-02-04 15:48:28 -0500

    Add board and new game btn to main page with css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import actions from "./actions";
+import Board from "./components/Board";
 
 function App() {
+  const curPlayer = useSelector(state => state?.gameReducers?.curPlayer);
+  const winner = useSelector(state => state?.gameReducers?.winner);
+  const gameId = useSelector(state => state?.gameReducers?.gameId);
+  const isTie = useSelector(state => state?.gameReducers?.isTie);
+
+  const dispatch = useDispatch();
+
+  const startGame = () => {
+    dispatch(actions?.gameAction?.startNewGame());
+  }
+
+  useEffect(() => {
+    startGame();
+  }, [])
+
   return (
     <div className="App">
-      App
+      <h1>Tic Tac Toe</h1>
+      <Board />
+      <button className='newGameBtn' onClick={startGame}>
+        Start New Game
+      </button>
     </div>
   );
 }
