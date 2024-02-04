8e04609d072ab937957dccd45e4be7ca67ef1beb
Daisy Wu <daisy43097@gmail.com>
2024-02-04 13:34:13 -0500

    Implement game reducers

--- src/reducers/gameReducers.js
+++ src/reducers/gameReducers.js
@@ -1,9 +1,17 @@
-const initState = {
+import {actionType, player} from "../helper/const";
 
+const initState = {
+  board: Array(9).fill(null),
+  curPlayer: player.x,
+  winner: null,
+  gameId: null,
+  isTie: false,
 };
 
 export const gameReducers = (state = initState, action) => {
   switch (action.type) {
+    case actionType.START_NEW_GAME:
+      return {...state, gameId: action.payload};
     default:
       return state;
   }
